4998343f53e13332e47b44d1f1fbeb1f4d09bf92
upi1682 <upi538287@gmail.com>
2026-07-29 15:51:36 +0700

    add genre routes with authentication middleware

--- routes/api.js
+++ routes/api.js
@@ -16,6 +16,10 @@ router.post("/komik", authMiddleware, komikController.createKomik);
 router.put("/komik/:id", authMiddleware, komikController.updateKomik);
 router.delete("/komik/:id", authMiddleware, komikController.deleteKomik);
 
-
+router.get("/genre", authMiddleware, genreController.getAllGenre);
+router.get("/genre/:id", authMiddleware, genreController.getGenreById);
+router.post("/genre", authMiddleware, genreController.createGenre);
+router.put("/genre/:id", authMiddleware, genreController.updateGenre);
+router.delete("/genre/:id", authMiddleware, genreController.deleteGenre);
 
 module.exports = router;
